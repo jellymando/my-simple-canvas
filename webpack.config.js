93072ca20a428b78640b5031d7c8d9297fae2dc0
jellymando <cicikdi@naver.com>
2021-06-09 18:43:10 +0900

    feat: package, tsconfig, webpack.config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,32 @@
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: {
-        main: './src/index.tsx',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    },
-    output: {
-        path: resolve(__dirname, 'dist'),
-        filename: 'js/build.js',
-    },
+  entry: {
+    main: "./src/index.ts",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "index.js",
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: '/node_modules/',
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-            },
-            {
-                test: /\.jfif$/,
-                loader: 'file-loader',
-                options: {
-                    name: 'img/[name].[ext]',
-                },
-            },
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            minify: false,
-        }),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+      { test: /\.tsx?$/, loader: "ts-loader" },
     ],
-    optimization: {
-        minimizer: [new TerserPlugin({ extractComments: false })],
-    },
+  },
+  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })],
+  },
 };
