97edf2f5f1f15827aee2e35351bb806a8d477e4c
jellymando <cicikdi@naver.com>
2021-06-09 13:11:44 +0900

    feat: drawOn, isDrawing 구분

--- src/Painter/index.ts
+++ src/Painter/index.ts
@@ -1,150 +1,168 @@
-import EventEmitter, { Listener } from './EventEmitter';
-import { EventMap, PainterOption, FigureData, RelativePosition } from './types';
-import { storage, getDrawingOn, getFigures } from '../lib/storage';
+// import EventEmitter, { Listener } from './EventEmitter';
+import EventEmitter from "events";
+import { EventMap, PainterOption, FigureData, RelativePosition } from "./types";
+import { storage, getDrawOn, getFigures } from "../lib/storage";
 
 export default class Painter {
-    private canvas: null | HTMLCanvasElement;
-    private ctx: null | CanvasRenderingContext2D;
-    private isDrawing: boolean;
-    private strokeColor: string | CanvasGradient | CanvasPattern;
-    private thickness: number;
-    private positions: RelativePosition[];
-    private figures: FigureData[];
-    private emitter: EventEmitter;
-    private removeDrawEvent: () => void;
+  private canvas: null | HTMLCanvasElement;
+  private ctx: null | CanvasRenderingContext2D;
+  private drawOn: boolean;
+  private isDrawing: boolean;
+  private strokeColor: string | CanvasGradient | CanvasPattern;
+  private thickness: number;
+  private positions: RelativePosition[];
+  private figures: FigureData[];
+  private emitter: EventEmitter;
+  private removeDrawEvent: () => void;
 
-    constructor({ color, thickness }: PainterOption) {
-        this.canvas = null;
-        this.ctx = null;
-        this.isDrawing = getDrawingOn();
-        this.strokeColor = color || 'red';
-        this.thickness = thickness || 3;
-        this.positions = [];
-        this.figures = getFigures();
-        this.emitter = new EventEmitter();
-        this.removeDrawEvent = () => {};
+  constructor({ color, thickness }: PainterOption) {
+    this.canvas = null;
+    this.ctx = null;
+    this.drawOn = getDrawOn();
+    this.isDrawing = false;
+    this.strokeColor = color || "red";
+    this.thickness = thickness || 3;
+    this.positions = [];
+    this.figures = getFigures();
+    this.emitter = new EventEmitter();
+    this.removeDrawEvent = () => {};
+  }
+
+  setTarget(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
+    this.addDrawEvent();
+    if (this.drawOn && this.figures.length > 0) this.redraw();
+  }
+
+  on(
+    name: "drawStart" | "draw" | "drawEnd" | "figures",
+    listener: (...args: any[]) => void
+  ) {
+    return this.emitter.on(name, listener);
+  }
+
+  add<Event extends keyof EventMap<HTMLCanvasElement>>(
+    name: Event,
+    callback: (event: EventMap<HTMLCanvasElement>[Event]) => void
+  ) {
+    if (!this.canvas) return;
+    this.canvas.addEventListener(name, callback);
+
+    return () => {
+      this.canvas!.removeEventListener(name, callback);
+    };
+  }
+
+  drawStart(painterOption?: PainterOption) {
+    if (!painterOption)
+      painterOption = { color: this.strokeColor, thickness: this.thickness };
+    this.isDrawing = true;
+    this.setOptions(painterOption);
+  }
+
+  draw(position: RelativePosition, redraw = false) {
+    if (!this.ctx) return;
+
+    const drawX = position!.x;
+    const drawY = position!.y;
+
+    if (!this.isDrawing) {
+      this.ctx.beginPath();
+    } else {
+      this.ctx.lineTo(drawX, drawY);
+      this.ctx.stroke();
     }
-
-    setTarget(canvas: HTMLCanvasElement) {
-        this.canvas = canvas;
-        this.ctx = canvas.getContext('2d');
-        this.addDrawEvent();
-        if (this.isDrawing && this.figures.length > 0) this.redraw();
+    if (!redraw) {
+      this.positions.push(position);
+      this.emitter.emit("draw", position);
     }
+  }
 
-    on(name: 'drawStart' | 'draw' | 'drawEnd' | 'figures', listener: Listener) {
-        return this.emitter.on(name, listener);
+  drawEnd(redraw = false) {
+    this.isDrawing = false;
+    this.ctx!.beginPath();
+    if (!redraw) {
+      this.setFigures();
+      this.emitter.emit("drawEnd");
     }
+  }
 
-    add<Event extends keyof EventMap<HTMLCanvasElement>>(
-        name: Event,
-        callback: (event: EventMap<HTMLCanvasElement>[Event]) => void
-    ) {
-        if (!this.canvas) return;
-        this.canvas.addEventListener(name, callback);
-
-        return () => {
-            this.canvas!.removeEventListener(name, callback);
-        };
+  redraw() {
+    for (const figure of this.figures) {
+      this.drawStart(figure.painterOption);
+      figure.positions.forEach((position) => {
+        this.draw(position, true);
+      });
+      this.drawEnd(true);
     }
+  }
 
-    drawStart() {
-        this.isDrawing = true;
-        this.setOptions({ color: this.strokeColor, thickness: this.thickness });
-    }
+  setOptions({ color, thickness }: PainterOption) {
+    if (!this.ctx) return;
+    this.ctx.strokeStyle = color;
+    this.ctx.lineWidth = thickness;
+  }
 
-    draw(position: RelativePosition, redraw = false) {
-        if (!this.ctx) return;
+  getFigures() {
+    this.figures = getFigures();
+  }
 
-        const drawX = position!.x;
-        const drawY = position!.y;
+  setFigures() {
+    this.figures.push({
+      painterOption: { color: this.strokeColor, thickness: this.thickness },
+      positions: this.positions,
+    });
+    storage.set("figures", this.figures);
+    this.positions = [];
+    this.emitter.emit("figures");
+  }
 
-        if (!this.isDrawing) {
-            this.ctx.beginPath();
-        } else {
-            this.ctx.lineTo(drawX, drawY);
-            this.ctx.stroke();
-        }
-        if (!redraw) {
-            this.positions.push(position);
-            this.emitter.emit('draw', position);
-        }
-    }
+  addDrawEvent() {
+    if (!this.canvas) return;
 
-    drawEnd() {
-        this.isDrawing = false;
-        this.ctx!.beginPath();
-        this.setFigures();
-        this.emitter.emit('drawEnd');
-    }
+    const handleMouseMove = (e: MouseEvent) => {
+      const position = { x: e.clientX, y: e.clientY };
+      this.draw(position);
+    };
 
-    redraw() {
-        for (const figure of this.figures) {
-            this.setOptions(figure.painterOption);
-            figure.positions.forEach((position) => {
-                this.draw(position, true);
-            });
-            this.ctx!.beginPath();
-        }
-    }
+    const handleTouchMove = (e: TouchEvent) => {
+      if (e.target === this.canvas) {
+        e.preventDefault();
+      }
+      const rect = this.canvas!.getBoundingClientRect();
+      const position = {
+        x: e.touches[0].clientX - rect.left,
+        y: e.touches[0].clientY - rect.top,
+      };
+      this.draw(position);
+    };
 
-    setOptions({ color, thickness }: PainterOption) {
-        if (!this.ctx) return;
-        this.ctx.strokeStyle = color;
-        this.ctx.lineWidth = thickness;
-    }
+    const canvasEvents = [
+      this.add("mousedown", () => this.drawStart()),
+      this.add("mousemove", (e) => handleMouseMove(e)),
+      this.add("mouseup", () => this.drawEnd()),
+      this.add("mouseleave", () => this.drawEnd()),
+      this.add("touchstart", () => this.drawStart()),
+      this.add("touchmove", (e) => handleTouchMove(e)),
+      this.add("touchend", () => this.drawEnd()),
+    ];
 
-    getFigures() {
-        this.figures = getFigures();
-    }
+    this.removeDrawEvent = () => canvasEvents.forEach((off) => off!());
+  }
 
-    setFigures() {
-        this.figures.push({
-            painterOption: { color: this.strokeColor, thickness: this.thickness },
-            positions: this.positions,
-        });
-        storage.set('figures', this.figures);
-        this.positions = [];
-        this.emitter.emit('figures');
-    }
+  clear() {
+    this.ctx!.clearRect(
+      0,
+      0,
+      this.canvas!.clientWidth,
+      this.canvas!.clientHeight
+    );
+  }
 
-    addDrawEvent() {
-        if (!this.canvas) return;
-
-        const handleMouseMove = (e: MouseEvent) => {
-            const position = { x: e.clientX, y: e.clientY };
-            this.draw(position);
-        };
-
-        const handleTouchMove = (e: TouchEvent) => {
-            if (e.target === this.canvas) {
-                e.preventDefault();
-            }
-            const rect = this.canvas!.getBoundingClientRect();
-            const position = { x: e.touches[0].clientX - rect.left, y: e.touches[0].clientY - rect.top };
-            this.draw(position);
-        };
-
-        const canvasEvents = [
-            this.add('mousedown', (e) => handleMouseMove(e)),
-            this.add('mousedown', () => this.drawStart()),
-            this.add('mouseup', () => this.drawEnd()),
-            this.add('mouseleave', () => this.drawEnd()),
-            this.add('touchstart', () => this.drawStart()),
-            this.add('touchmove', (e) => handleTouchMove(e)),
-            this.add('touchend', () => this.drawEnd()),
-        ];
-
-        this.removeDrawEvent = () => canvasEvents.forEach((off) => off!());
-    }
-
-    clear() {
-        this.ctx!.clearRect(0, 0, this.canvas!.clientWidth, this.canvas!.clientHeight);
-    }
-
-    destroy() {
-        this.removeDrawEvent();
-        this.emitter.allOff();
-        storage.remove('figures');
-    }
+  destroy() {
+    this.removeDrawEvent();
+    this.emitter.removeAllListeners();
+    storage.remove("figures");
+  }
 }
--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,51 +1,53 @@
-import { FigureData } from '../Painter/types';
+import { FigureData } from "../Painter/types";
 
-const ROOT_KEY = <const>'painter';
+const ROOT_KEY = <const>"painter";
 
 class Storage<T> {
-    private _items: T;
+  private _items: T;
 
-    constructor() {
-        this._items = JSON.parse(sessionStorage.getItem(ROOT_KEY) || JSON.stringify({ log: [] }));
-    }
+  constructor() {
+    this._items = JSON.parse(
+      sessionStorage.getItem(ROOT_KEY) || JSON.stringify({ log: [] })
+    );
+  }
 
-    get<K extends keyof T>(key: K): T[K] {
-        return this._items[key];
-    }
+  get<K extends keyof T>(key: K): T[K] {
+    return this._items[key];
+  }
 
-    all(): T {
-        return this._items;
-    }
+  all(): T {
+    return this._items;
+  }
 
-    set<K extends keyof T>(key: K, value: T[K]): void {
-        this._items[key] = value;
-        this._save();
-    }
+  set<K extends keyof T>(key: K, value: T[K]): void {
+    this._items[key] = value;
+    this._save();
+  }
 
-    remove(key: keyof T): void {
-        delete this._items[key];
-        this._save();
-    }
+  remove(key: keyof T): void {
+    delete this._items[key];
+    this._save();
+  }
 
-    private _save(): void {
-        sessionStorage.setItem(ROOT_KEY, JSON.stringify(this._items));
-    }
+  private _save(): void {
+    sessionStorage.setItem(ROOT_KEY, JSON.stringify(this._items));
+  }
 
-    clear(): void {
-        this._items = { log: [] } as any;
-        this._save();
-    }
+  clear(): void {
+    this._items = { log: [] } as any;
+    this._save();
+  }
 }
 
 export const storage = new Storage<{
-    drawingOn?: boolean;
-    figures?: FigureData[];
+  drawOn?: boolean;
+  figures?: FigureData[];
 }>();
 
-export const getDrawingOn = () => {
-    return storage.get('drawingOn') || false;
+export const getDrawOn = () => {
+  return storage.get("drawOn") || false;
 };
 
 export const getFigures = () => {
-    return storage.get('figures') || [];
+  return storage.get("figures") || [];
 };
