88074ff2598653ca90c8b10b4f25cd5ac6b1f789
jellymando <cicikdi@naver.com>
2021-06-28 22:10:41 +0900

    feat: controlItem 아이콘 추가, painter save 메서드 추가, 불필요한 코드 삭제

--- src/Toolbar/ControlItem.tsx
+++ src/Toolbar/ControlItem.tsx
@@ -1,65 +1,38 @@
 import React, { ReactElement, useState } from "react";
 import { ItemWrapper, Round, Item } from "./styled";
-import { Icon } from "./Icons";
 
 type ControlItemProps = {
-  icon?: string;
   title?: string;
-  text?: string;
   active?: boolean;
   disable?: boolean;
-  dropdown?: ReactElement;
-  activeDropdown?: boolean;
-  hidden?: boolean;
-  palette?: string;
-  color?: string;
-  onSelectItem?: (item: string) => void;
+  onClickItem?: () => void;
   onClick?: () => void;
+  children?: any;
 };
 
 export const ControlItem = ({
-  icon,
   title,
-  text,
   active,
   disable,
-  dropdown,
-  activeDropdown,
-  hidden,
-  palette,
-  color,
-  onSelectItem,
+  onClickItem,
   onClick = () => null,
+  children
 }: ControlItemProps) => {
-  const [openDropdown, setOpenDropdown] = useState(false);
-  const toggleDropdown = () => setOpenDropdown(!openDropdown);
-  const onCloseDropdown = () => setOpenDropdown(false);
-
-  return !hidden ? (
-    <ItemWrapper title={title || icon}>
+  return (
+    <ItemWrapper title={title}>
       <Item
-        color={color}
         active={active}
         disable={disable}
         onClick={() => {
-          if (onSelectItem) {
-            if (icon) onSelectItem(icon);
-            if (color) onSelectItem("palette");
+          if (onClickItem) {
+            return onClickItem();
           }
-
-          if (activeDropdown) {
-            onClick();
-            return;
-          }
-
-          if (dropdown) toggleDropdown();
-          else onClick();
+          onClick();
         }}
         role="button"
       >
-        {icon && <Icon name={icon} />}
-        {color && <Round color={color} />}
+        {children}
       </Item>
     </ItemWrapper>
-  ) : null;
+  );
 };
--- src/Toolbar/Icons/Icon.tsx
+++ src/Toolbar/Icons/Icon.tsx
@@ -10,7 +10,7 @@ export const Icon = ({ name: iconName }: IconProps) => {
   switch (iconName) {
     case "allClear":
       return <AllClear />;
-    case "draw":
+    case "save":
       return <Save />;
     default:
       return <></>;
